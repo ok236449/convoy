32e6c29b61360c50b8023541fc35e0f0e0bd5958
ok236449 <89067167+ok236449@users.noreply.github.com>
2023-06-18 22:05:19 +0200

    stop from modifying ratelimits on all VMs

--- app/Services/Nodes/ServerRateLimitsSyncService.php
+++ app/Services/Nodes/ServerRateLimitsSyncService.php
@@ -21,9 +21,11 @@ class ServerRateLimitsSyncService
             try {
                 if ($server->bandwidth_usage >= $server->bandwidth_limit && isset($server->bandwidth_limit)) {
                     $this->service->updateRateLimit($server, 1);
-                } else {
-                    $this->service->updateRateLimit($server);
                 }
+                //dont modify other VMs
+                //else {
+                //    $this->service->updateRateLimit($server);
+                //}
             } catch (ProxmoxConnectionException $e) {
                 // do nothing
             }
